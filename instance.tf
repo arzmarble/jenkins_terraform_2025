2b5791e22f1ba60a9f92136fa0980f8a1488759f
arzmarble <thanadol.rungrot@gmail.com>
2025-12-18 00:18:20 +0700

    Update VPC + EC2

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
-# Create EC2 Instance
-resource "aws_instance" "new-ec2" {
+# Create EC2 Instance Public 1
+resource "aws_instance" "ec2-pub1" {
     ami = var.ami_id[var.AZ_1]
     instance_type = var.instnace_type
     subnet_id = aws_subnet.pub-1.id
@@ -7,12 +7,31 @@ resource "aws_instance" "new-ec2" {
     vpc_security_group_ids = [aws_security_group.UAJ-sg.id]
     key_name = aws_key_pair.key_pair.key_name
     tags = {
-        Name    = "T-Instance"
+        Name    = "T1-Instance"
         Project = "Test"
     }
 }
 
-# Print PublicIP
-output "PublicIP-Linux2" {
-  value = aws_instance.new-ec2.public_ip
+# Print PublicIP 1
+output "PublicIP-Linux-1" {
+  value = aws_instance.ec2-pub1.public_ip
+}
+
+# Create EC2 Instance Public 2
+resource "aws_instance" "ec2-pub2" {
+    ami = var.ami_id[var.AZ_2]
+    instance_type = var.instnace_type
+    subnet_id = aws_subnet.pub-2.id
+    associate_public_ip_address = true
+    vpc_security_group_ids = [aws_security_group.UAJ-sg.id]
+    key_name = aws_key_pair.key_pair.key_name
+    tags = {
+        Name    = "T2-Instance"
+        Project = "Test"
+    }
+}
+
+# Print PublicIP 2
+output "PublicIP-Linux-2" {
+  value = aws_instance.ec2-pub2.public_ip
 }
--- vpc.tf
+++ vpc.tf
@@ -56,9 +56,38 @@ resource "aws_route" "UAJ-Route" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-# Route with Public
+# Route with Public 1
 resource "aws_route_table_association" "IGW-Public-1" {
   subnet_id = aws_subnet.pub-1.id
   route_table_id = aws_route_table.GAJ-IGW-Public.id
 }
 
+
+### Pub & Pri Subnet
+# Public 2
+resource "aws_subnet" "pub-2" {
+  vpc_id                  = aws_vpc.GAJ.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = var.AZ_2
+  tags = {
+    Name = "GAJ-pub-2"
+  }
+}
+
+# Private 2
+resource "aws_subnet" "priv-2" {
+  vpc_id                  = aws_vpc.GAJ.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = var.AZ_2
+  tags = {
+    Name = "GAJ-priv-2"
+  }
+}
+
+# Route with Public 
+resource "aws_route_table_association" "IGW-Public-2" {
+  subnet_id = aws_subnet.pub-2.id
+  route_table_id = aws_route_table.GAJ-IGW-Public.id
+}
